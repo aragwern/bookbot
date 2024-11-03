dd5193ed40b3e72aec80f8f564c804d40b075218
Jaroslav Hůna <jaroslav@huna.dev>
2024-11-03 12:52:06 +0100

    book report

--- main.py
+++ main.py
@@ -1,19 +1,59 @@
-def read_book(file_path):
+def open_book(file_path):
     with open(file_path) as f:
         file_contents = f.read()
-        print(file_contents)
+        return file_contents
 
-def count_words(file_path):
-    with open(file_path) as f:
-        file_contents = f.read()
-        words = file_contents.split()
-        count = 0
-        for word in words:
-            count += 1
+
+def read_book(file_path):
+    file_contents = open_book(file_path)
+    print(file_contents)
+
+
+def count_words(text):
+    words = text.split()
+    count = 0
+    for word in words:
+        count += 1
     return count
 
+
+def count_chars(text):
+    chars = {}
+    for char in text:
+        char = char.lower()
+        if char in chars and char.isalpha():
+            chars[char] += 1
+        elif char.isalpha():
+            chars[char] = 1
+    return chars
+
+def sort_on(dict):
+    return dict["count"]
+
+def sort_char_count(char_dict):
+    char_list = []
+    for key in char_dict:
+        char_list.append({"char": key, "count": char_dict[key]})
+    return char_list
+
+def report(file_path):
+    text = open_book(file_path)
+    word_count = count_words(text)
+    char_count = count_chars(text)
+    char_count_list = sort_char_count(char_count)
+    char_count_list.sort(reverse=True,key=sort_on)
+    print(f"--- Begin report of {file_path} ---")
+    print(f"{word_count} words found in the document")
+    print("") # one empty line
+    for entry in char_count_list:
+        print(f"The '{entry["char"]}' character was found {entry["count"]} times")
+    print("--- End report ---")
+
 def main():
-    #read_book(r"books/frankenstein.txt")
-    print(count_words(r"books/frankenstein.txt"))
+    file_path = r"books/frankenstein.txt"
+    #read_book(file_path)
+    #print(count_words(file_path))
+    #print(count_chars(open_book(file_path)))
+    report(file_path)
 
 main()
