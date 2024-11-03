de2b2706062b0108a05f02ce437d2bf5f3c7fab3
Jaroslav Hůna <jaroslav@huna.dev>
2024-11-03 11:17:21 +0100

    count words

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
-def main():
-    with open(r"books/frankenstein.txt") as f:
+def read_book(file_path):
+    with open(file_path) as f:
         file_contents = f.read()
         print(file_contents)
+
+def count_words(file_path):
+    with open(file_path) as f:
+        file_contents = f.read()
+        words = file_contents.split()
+        count = 0
+        for word in words:
+            count += 1
+    return count
+
+def main():
+    #read_book(r"books/frankenstein.txt")
+    print(count_words(r"books/frankenstein.txt"))
+
 main()
